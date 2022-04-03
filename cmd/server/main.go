50713cfb9d2c851bff555d1b6fc8b2b32db4cae5
alex.zhou.hi.man <alex.zhou.hi.man@gmail.com>
2022-04-03 11:59:36 +0800

    chore: 更新版本号

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -18,7 +18,7 @@ import (
 
 const (
 	Name          = "trojan-node"
-	Version       = "0.1.4"
+	Version       = "0.1.5"
 	CopyRight     = "XFLASH-PANDA@2021"
 	LogLevelDebug = "debug"
 	LogLevelError = "error"
