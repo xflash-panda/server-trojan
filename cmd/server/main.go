b84f543ac1bc93f703cb9220aacd37afc4bad1e5
wwnice-max <wwnice-max@outlook.com>
2023-12-01 04:52:03 +0800

    chore: update

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -18,7 +18,7 @@ import (
 
 const (
 	Name          = "trojan-node"
-	Version       = "0.1.14"
+	Version       = "0.1.15"
 	CopyRight     = "XFLASH-PANDA@2021"
 	LogLevelDebug = "debug"
 	LogLevelError = "error"
